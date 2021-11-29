ecbbb6199b2e618106be64845063d05e9bbd0fc1
Greg <gregwenshell@gmail.com>
2021-11-29 10:54:34 +0100

    Greg

--- Tableview_JavaFX_Movies/application/MovieAppMain.java
+++ Tableview_JavaFX_Movies/application/MovieAppMain.java
@@ -1,20 +1,20 @@
 package application;
 
-import javafx.application.*; 
-import javafx.stage.*; 
-import javafx.scene.*; 
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
 import domain.MovieCompany;
-import ui.MovieOverview;
+
 import ui.MovieOverview2;
 
 public class MovieAppMain extends Application {
-    public static void main(String[] args)    {        
-    	launch(args);    
+    public static void main(String[] args)    {
+    	launch(args);
     }
-    
+
     @Override
 	public void start(Stage primaryStage) {
-    	MovieCompany movieCompany = new MovieCompany();	
+    	MovieCompany movieCompany = new MovieCompany();
 		//MovieOverview view = new MovieOverview(movieCompany);
 		MovieOverview2 view = new MovieOverview2(movieCompany);
 	}
