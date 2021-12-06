44ef53afa7480b7f279db01a676fba47d70b2a81
jelle-michiels <jelle.michiels1@student.ucll.be>
2021-12-06 21:21:56 +0100

    OrderMainPane werkend gekregen

--- src/view/orderMainPane/OrderMainPane.java
+++ src/view/orderMainPane/OrderMainPane.java
@@ -1,17 +1,16 @@
 package view.orderMainPane;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import model.OrderFacade;
-import view.adminPane.SandwichOverviewPane;
-import view.adminPane.ToppingOverviewPane;
 
 public class OrderMainPane extends GridPane {
     public OrderMainPane(OrderFacade orderFacade){
-        OrderHeader orderHeader = new OrderHeader();
-        this.getChildren().add(orderHeader);
+        FlowPane header = new FlowPane();
+        header.getChildren().addAll(new Button("New order"), new Label("Follownr:"), new ComboBox());
+        this.getChildren().addAll(header);
+
+
     }
 }
