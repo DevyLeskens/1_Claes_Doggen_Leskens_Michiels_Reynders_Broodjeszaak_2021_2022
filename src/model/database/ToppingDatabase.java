2fca6868bcedc0df596a0325b3839bc1eda4f11e
Greg <gregwenshell@gmail.com>
2021-12-12 22:50:10 +0100

    Factory statische maken

--- src/model/database/ToppingDatabase.java
+++ src/model/database/ToppingDatabase.java
@@ -31,7 +31,7 @@ public class ToppingDatabase {
 
     public void Load() {
         try {
-            this.toppingsorts = new LoadSaveStrategyFactory().createLoadSaveStrategy(LoadSaveStrategyEnum.TEXT_TOPPING).load();
+            this.toppingsorts = LoadSaveStrategyFactory.createLoadSaveStrategy(LoadSaveStrategyEnum.TEXT_TOPPING).load();
         } catch (Exception e){ System.out.println(e.getMessage()); }
     }
     // dummy methode
